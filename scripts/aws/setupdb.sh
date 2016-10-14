a1d4b0d5498e9ffdeb7c6a7e0af63381af5cb57f
Kelly Innes <kinnes@azavea.com>
2016-10-14 16:45:12 -0400

    Remove layer tables from setupdb.sh script
    - cut everything except a single placeholder file loading if/fi
      statement to use as a model for future data imports

--- scripts/aws/setupdb.sh
+++ scripts/aws/setupdb.sh
@@ -3,45 +3,27 @@
 set -e
 set -x
 
-usage="$(basename "$0") [-h] [-b] [-s] \n
---Sets up a postgresql database for MMW \n
+usage="$(basename "$0") [-h] [-b] [-f] \n
+--Sets up a postgresql database \n
 \n
 where: \n
     -h  show this help text\n
-    -b  load/reload boundary data\n
-    -f  load a named boundary sql.gz\n
-    -s  load/reload stream data\n
-    -d  load/reload DRB stream data\n
-    -m  load/reload mapshed data\n
-    -p  load/reload DEP data\n
-    -q  load/reload water quality data\n
+    -b  load/reload mapshed data\n
+    -f  load/reload a specific file\n
 "
 
 # HTTP accessible storage for initial app data
 FILE_HOST="https://s3.amazonaws.com/data.mmw.azavea.com"
-load_boundary=false
 file_to_load=
-load_stream=false
-load_mapshed=false
-load_water_quality=false
+load_bee_data=false
 
-while getopts ":hbsdpmqf:" opt; do
+while getopts ":hmf:" opt; do
     case $opt in
         h)
             echo -e $usage
             exit ;;
         b)
-            load_boundary=true ;;
-        s)
-            load_stream=true ;;
-        d)
-            load_drb_streams=true ;;
-        p)
-            load_dep=true ;;
-        m)
-            load_mapshed=true ;;
-        q)
-            load_water_quality=true ;;
+            load_bee_data=true ;;
         f)
             file_to_load=$OPTARG ;;
         \?)
@@ -75,46 +57,9 @@ if [ ! -z "$file_to_load" ] ; then
     download_and_load $FILES
 fi
 
-if [ "$load_dep" = "true" ] ; then
-    # Fetch DEP layers
-    FILES=("dep_urban_areas.sql.gz" "dep_municipalities.sql.gz")
-
-    download_and_load $FILES
-fi
-
-
-if [ "$load_boundary" = "true" ] ; then
-    # Fetch boundary layer sql files
-    FILES=("boundary_county.sql.gz" "boundary_school_district.sql.gz" "boundary_district.sql.gz" "boundary_huc12.sql.gz" "boundary_huc10.sql.gz" "boundary_huc08.sql.gz")
-
-    download_and_load $FILES
-fi
-
-if [ "$load_stream" = "true" ] ; then
-    # Fetch stream network layer sql files
-    FILES=("nhdflowline.sql.gz")
-
-    download_and_load $FILES
-fi
-
-if [ "$load_drb_streams" = "true" ] ; then
-    # Fetch DRB stream network layer sql file
-    FILES=("drb_streams_50.sql.gz")
-
-    download_and_load $FILES
-fi
-
-if [ "$load_mapshed" = "true" ] ; then
+if [ "$load_bee_data" = "true" ] ; then
     # Fetch map shed specific vector features
-    FILES=("ms_weather.sql.gz" "ms_weather_station.sql.gz" "ms_pointsource.sql.gz"
-           "ms_pointsource_drb.sql.gz" "ms_county_animals.sql.gz")
-
-    download_and_load $FILES
-fi
-
-if [ "$load_water_quality" = "true" ] ; then
-    # Fetch water quality data
-    FILES=("nhd_water_quality.sql.gz" "drb_catchment_water_quality.sql.gz")
+    FILES=("bee_pollinator_data.sql.gz")
 
     download_and_load $FILES
 fi
