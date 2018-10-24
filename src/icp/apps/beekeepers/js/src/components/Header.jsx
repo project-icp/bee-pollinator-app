006434026a448f52b8ee078a8d60725b7d07da1f
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-10-24 16:10:09 -0400

    Copy prototype into simple components

---
+++ src/icp/apps/beekeepers/js/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default () => (
+    <header className="header">
+        <div className="navbar">
+            <div className="navbar__logo">
+                Landscape4Bees
+            </div>
+            <ul className="navbar__items">
+                <li className="navbar__item">
+                    <button className="navbar__button">
+                        About
+                    </button>
+                </li>
+                <li className="navbar__item">
+                    <button className="navbar__button">
+                        Methodology
+                    </button>
+                </li>
+                <li className="navbar__item">
+                    <button className="navbar__button">
+                        Account
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </header>
+);
