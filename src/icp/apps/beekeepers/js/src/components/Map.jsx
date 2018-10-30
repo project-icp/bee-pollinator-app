df6691f48be2f430d3c946f0f34eae32eef6486c
Jenny Fung <jfung@azavea.com>
2018-10-30 15:59:55 -0400

    Add react leaflet map with OSM basemap
    The OSM basemap is temporary.
    
    Connects #298

--- src/icp/apps/beekeepers/js/src/components/Map.jsx
+++ src/icp/apps/beekeepers/js/src/components/Map.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+import { arrayOf, number } from 'prop-types';
+import { connect } from 'react-redux';
 
-export default function () {
-    return (<div className="map" />);
+const Map = ({ mapCenter, zoom }) => (
+    <div className="map">
+        <LeafletMap
+            center={mapCenter}
+            zoom={zoom}
+        >
+            <TileLayer
+                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </LeafletMap>
+    </div>
+);
+
+function mapStateToProps(state) {
+    return state.main;
 }
+
+Map.propTypes = {
+    mapCenter: arrayOf(number).isRequired,
+    zoom: number.isRequired,
+};
+
+export default connect(mapStateToProps)(Map);
--- src/icp/apps/beekeepers/js/src/constants.js
+++ src/icp/apps/beekeepers/js/src/constants.js
@@ -2,3 +2,6 @@ export const FORAGE_RANGE_3K = '3k';
 export const FORAGE_RANGE_5K = '5k';
 
 export const SORT_CREATED = 'SORT_CREATED';
+
+export const MAP_CENTER = [37.899, -97.079];
+export const MAP_ZOOM = 10;
