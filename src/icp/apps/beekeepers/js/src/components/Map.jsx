12777b3bf6bf6043141776c3990407f3d5286d58
Jenny Fung <jfung@azavea.com>
2018-10-31 16:08:49 -0400

    Add click to map
    Refs #300

--- src/icp/apps/beekeepers/js/src/components/Map.jsx
+++ src/icp/apps/beekeepers/js/src/components/Map.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 import { connect } from 'react-redux';
+import { arrayOf, object } from 'prop-types';
 
 import { MAP_CENTER, MAP_ZOOM } from '../constants';
 
-const Map = () => (
-    <div className="map">
-        <LeafletMap
-            center={MAP_CENTER}
-            zoom={MAP_ZOOM}
-        >
-            <TileLayer
-                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-        </LeafletMap>
-    </div>
-);
+class Map extends Component {
+    constructor() {
+        super();
+        this.onClickAddMarker = this.onClickAddMarker.bind(this);
+    }
+
+    onClickAddMarker(event) {
+        window.console.log(event);
+        const { apiaries } = this.props;
+        apiaries.push({
+            name: 'Geocoded name',
+            location: event.latlng,
+        });
+    }
+
+    render() {
+        return (
+            <div className="map">
+                <LeafletMap
+                    center={MAP_CENTER}
+                    zoom={MAP_ZOOM}
+                    onClick={this.onClickAddMarker}
+                >
+                    <TileLayer
+                        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                </LeafletMap>
+            </div>
+        );
+    }
+}
 
 function mapStateToProps(state) {
     return state.main;
 }
 
-Map.propTypes = {};
+Map.propTypes = {
+    apiaries: arrayOf(object).isRequired,
+};
 
 export default connect(mapStateToProps)(Map);
