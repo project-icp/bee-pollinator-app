1ae332d0287e7a07eddae5a576069848079f3245
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-10-31 15:26:47 -0400

    Use Apiary Card in Sidebar
    Add placeholder for Forage Range, Sort selectors.
    Also remove the Prototype, which has now been completely
    subsumed by other components.

--- src/icp/apps/beekeepers/js/src/components/Sidebar.jsx
+++ src/icp/apps/beekeepers/js/src/components/Sidebar.jsx
@@ -4,14 +4,45 @@ import { connect } from 'react-redux';
 
 import { Apiary } from '../propTypes';
 
+import ApiaryCard from './ApiaryCard';
 import Splash from './Splash';
-import Prototype from './Prototype';
 
-const Sidebar = ({ apiaries }) => (
-    apiaries.length > 0
-        ? <Prototype />
-        : <Splash />
-);
+const Sidebar = ({ apiaries }) => {
+    if (apiaries.length === 0) {
+        return <Splash />;
+    }
+
+    const apiaryCards = apiaries.map(apiary => (
+        <ApiaryCard key={apiary.marker} apiary={apiary} />
+    ));
+
+    return (
+        <div className="sidebar">
+            <h2 className="sidebar__header">Locations</h2>
+            <div className="controls">
+                {/* TODO Implement Forage Range, Sort selectors
+                    https://github.com/project-icp/bee-pollinator-app/issues/316 */}
+                <div className="controls__select">
+                    <select>
+                        <option value="one">one</option>
+                        <option value="two">two</option>
+                        <option value="three">three</option>
+                    </select>
+                </div>
+                <div className="controls__select">
+                    <select>
+                        <option value="one">one</option>
+                        <option value="two">two</option>
+                        <option value="three">three</option>
+                    </select>
+                </div>
+            </div>
+            <ul className="card-container">
+                {apiaryCards}
+            </ul>
+        </div>
+    );
+};
 
 Sidebar.propTypes = {
     apiaries: arrayOf(Apiary).isRequired,
