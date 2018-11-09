8cb74a27708cdd02180b4a8cde0d53eb72d831fd
Jenny Fung <jfung@azavea.com>
2018-11-09 11:15:59 -0500

    Capitalize sort filters
    Works on IE11, Chrome, Safari but not Firefox which capitalizes the selection but not the option text
    Refs #316

--- src/icp/apps/beekeepers/js/src/constants.js
+++ src/icp/apps/beekeepers/js/src/constants.js
@@ -2,7 +2,7 @@ export const FORAGE_RANGE_3KM = '3km';
 export const FORAGE_RANGE_5KM = '5km';
 
 export const FORAGE_RANGES = [FORAGE_RANGE_3KM, FORAGE_RANGE_5KM];
-export const DEFAULT_SORT = 'Default';
+export const DEFAULT_SORT = 'default';
 
 export const INDICATORS = {
     NESTING_QUALITY: 'nesting_quality',
