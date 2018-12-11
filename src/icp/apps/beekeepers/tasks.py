03e9d9d461a5971460f6596c2de23972fe29bdf8
Matthew McFarland <mmcfarland@azavea.com>
2018-12-11 16:38:11 -0500

    Access rasters via a VRT
    Rasters are now provided as discreet files representing single states.
    In order to mosaic them on the fly and read from the correct file, VRTs
    have been created which reference adjacent raw geotiffs. Since rasterio
    delegates vsis3 calls directly to gdal for the path, additional work
    must be done to provide AWS credentials via environment variables as
    rasterio will only continue to read the initial VRT read with it's own
    configured credentials.

--- src/icp/apps/beekeepers/tasks.py
+++ src/icp/apps/beekeepers/tasks.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
+import os
 
+import boto3
 import rasterio
 from pyproj import Proj
 
@@ -17,21 +19,69 @@ def sample_at_point(geom, raster_path):
     :type geom: Dict with lat and lon values, like {lat: float, lon: float}
     :type raster_path: String
     """
-    try:
-        with rasterio.open(raster_path, mode='r') as src:
-            # reproject latlon to coords in the same SRS as the target raster
-            to_proj = Proj(src.crs)
-            x, y = to_proj(geom['lng'], geom['lat'])
+    with gdal_configured_credentials():
+        try:
+            with rasterio.open(raster_path, mode='r') as src:
+                # Reproject latlon to coords in the same SRS as the
+                # target raster
+                to_proj = Proj(src.crs)
+                x, y = to_proj(geom['lng'], geom['lat'])
 
-            # Sample the raster at the given coordinates
-            value_gen = src.sample([(x, y)], indexes=[1])
-            value = value_gen.next().item(0)
-    except rasterio.RasterioIOError as e:
-        return ({
-            "data": None,
-            "error": e.message
-        })
+                # Sample the raster at the given coordinates
+                value_gen = src.sample([(x, y)], indexes=[1])
+                value = value_gen.next().item(0)
+        except rasterio.RasterioIOError as e:
+            return ({
+                "data": None,
+                "error": e.message
+            })
+
     return ({
         "data": value,
         "error": None
     })
+
+
+class gdal_configured_credentials():
+    """
+    A context manager which ensures any environment variables containing AWS
+    credentials are cleared after the context is closed.
+    These must be set again in order for GDAL vsis3 file handlers to work, but
+    when running in production, their presence affects how boto will acquire
+    new credentials.
+    """
+
+    def remove_env_vars(self):
+        """Clear all AWS environment variables"""
+
+        if 'AWS_ACCESS_KEY_ID' in os.environ:
+            del os.environ['AWS_ACCESS_KEY_ID']
+
+        if 'AWS_SECRET_ACCESS_KEY' in os.environ:
+            del os.environ['AWS_SECRET_ACCESS_KEY']
+
+        if 'AWS_SESSION_TOKEN' in os.environ:
+            del os.environ['AWS_SESSION_TOKEN']
+
+    def create_new_env_vars(self):
+        """
+        Fetch new AWS credentials and supply them to the environment variables
+        which GDAL will use for vsis3 requests
+        """
+        credentials = boto3.Session().get_credentials()
+        os.environ['AWS_ACCESS_KEY_ID'] = credentials.access_key
+        os.environ['AWS_SECRET_ACCESS_KEY'] = credentials.secret_key
+
+        # In dev, the token won't be available. In prod, this should be
+        # provided via the instance metadata
+        if credentials.token:
+            os.environ['AWS_SESSION_TOKEN'] = credentials.token
+
+    def __enter__(self):
+        """Context manager start"""
+        self.remove_env_vars()
+        self.create_new_env_vars()
+
+    def __exit__(self, *args):
+        """Context manager close"""
+        self.remove_env_vars()
