ca5896f2c517ed6fb892c25ea1fa6c07a4d8d989
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-12-11 14:09:23 -0500

    Add model for November survey

--- src/icp/apps/beekeepers/models.py
+++ src/icp/apps/beekeepers/models.py
@@ -120,3 +120,85 @@ class AprilSurvey(models.Model):
     colony_loss_reason = models.TextField(
         null=False,
         help_text='The most likely causes for colony loss')
+
+
+class NovemberSurvey(models.Model):
+    """
+    A long survey taken in November by all users.
+
+    - varroa_check_method and varroa_manage_frequency need only be filled if
+      varroa_check_frequency has been filled out
+    - mite_management can take a large list of predefined inputs, but also
+      free-form text prefixed by CHEMICAL_OTHER_ORGANIC-, MECHANICAL_OTHER-,
+      or OTHER-
+    """
+    HARVEST_CHOICES = (
+        ('DID_NOT_HARVEST', 'Did not harvest'),
+        ('LESS_THAN_10', 'Less than 10 lbs'),
+        ('BETWEEN_10_AND_25', 'Between 10 and 25 lbs'),
+        ('BETWEEN_25_AND_50', 'Between 25 and 50 lbs'),
+        ('MORE_THAN_50', 'More than 50 lbs')
+    )
+
+    FREQUENCY_CHOICES = (
+        ('NEVER', 'I did not'),
+        ('ONCE', 'Once a year'),
+        ('TWICE', 'Twice a year'),
+        ('THRICE', 'Three times a year'),
+        ('MORE_THAN_THREE', 'More than three times a year'),
+    )
+
+    survey = models.OneToOneField(
+        Survey,
+        primary_key=True,
+        related_name='november')
+    harvested_honey = models.CharField(
+        max_length=255,
+        null=False,
+        choices=HARVEST_CHOICES,
+        help_text='How much honey did you collect on average for each colony?')
+    supplemental_sugar = models.CharField(
+        # SPRING;SUMMER;FALL;WINTER
+        max_length=255,
+        null=True,
+        help_text='Did you feed supplemental sugar? '
+                  'If so, what times of year did you feed sugar? '
+                  'Check all that apply.')
+    supplemental_protein = models.CharField(
+        # SPRING;SUMMER;FALL;WINTER
+        max_length=255,
+        null=True,
+        help_text='Did you feed supplemental pollen or protein? '
+                  'If so, what times of year did you feed pollen or protein? '
+                  'Check all that apply.')
+    small_hive_beetles = models.BooleanField(
+        null=False,
+        help_text='Have you observed small hive beetles in your hives?')
+    varroa_check_frequency = models.CharField(
+        max_length=255,
+        null=False,
+        choices=FREQUENCY_CHOICES,
+        help_text='How often do you check for Varroa?')
+    varroa_check_method = models.CharField(
+        # ALCOHOL_WASH;SUGAR_SHAKE;STICKY_BOARDS;OTHER-
+        max_length=255,
+        null=True,
+        help_text='What method do you use to check for Varroa?')
+    varroa_manage_frequency = models.CharField(
+        max_length=255,
+        null=False,
+        choices=FREQUENCY_CHOICES,
+        default='NEVER',
+        help_text='Do you manage for Varroa? If so, how often?')
+    mite_management = models.TextField(
+        # CHEMICAL_FORMIC_ACID_MAQS;CHEMICAL_FORMIC_ACID_FORMIC_PRO;
+        # CHEMICAL_OXALIC_ACID_VAPORIZATION;CHEMICAL_OXALIC_ACID_DRIBBLE;
+        # CHEMICAL_THYMOL_MENTHOL_APILIFE;CHEMICAL_THYMOL_MENTHOL_APIGUARD;
+        # CHEMICAL_SYNTHETIC_APIVAR;CHEMICAL_SYNTHETIC_APISTAN;
+        # CHEMICAL_SYNTHETIC_CHECKMITE_PLUS;CHEMICAL_OTHER_ORGANIC-
+        # MECHANICAL_DRONE_BROOD_REMOVAL;MECHANICAL_QUEEN_MANIPULATION;
+        # MECHANICAL_OTHER-
+        # OTHER-
+        null=True,
+        help_text='What methods of mite management do you use? '
+                  'Select all that apply.')
