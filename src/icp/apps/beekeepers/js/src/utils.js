7e6a95c3519671ab227c87994489f2b2924ba03c
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-12-19 16:50:14 -0500

    Add marker sequence generator
    Given a marker, these helper methods generate the next in the
    alphabetical sequence, in the Excel style.
    
    getNextInSequence('A')  =>  'B'
    getNextInSequence('Z')  => 'AA'
    getNextInSequence('AZ') => 'BA'

--- src/icp/apps/beekeepers/js/src/utils.js
+++ src/icp/apps/beekeepers/js/src/utils.js
@@ -16,3 +16,44 @@ export function toTitleCase(value) {
 export function isSameLocation(a, b) {
     return a.lat === b.lat && a.lng === b.lng;
 }
+
+function isLastInSequence(s) {
+    let isLast = true;
+
+    [...s].forEach((c) => {
+        if (c !== 'Z') {
+            isLast = false;
+        }
+    });
+
+    return isLast;
+}
+
+function getNextLetter(c) {
+    const cc = c.charCodeAt(0);
+
+    if (cc < 90) {
+        return String.fromCharCode(cc + 1);
+    }
+
+    return 'A';
+}
+
+// Adapted from https://stackoverflow.com/a/34483399/6995854
+export function getNextInSequence(s) {
+    if (isLastInSequence(s)) {
+        return 'A'.repeat(s.length + 1);
+    }
+
+    const prefix = s.slice(0, -1);
+    const penultimate = prefix.slice(-1);
+    const final = s.slice(-1);
+
+    if (final === 'Z') {
+        return prefix.slice(0, -1)
+            + getNextLetter(penultimate)
+            + getNextLetter(final);
+    }
+
+    return prefix + getNextLetter(final);
+}
