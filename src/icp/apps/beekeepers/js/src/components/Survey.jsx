34c600345442f916a393105f82fcc7983db7b89f
Jenny Fung <jfung@azavea.com>
2018-12-23 10:46:30 -0500

    Break Survey view states into their own components
    Refs #330

--- src/icp/apps/beekeepers/js/src/components/Survey.jsx
+++ src/icp/apps/beekeepers/js/src/components/Survey.jsx
@@ -1,91 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
-import { arrayOf, number, func } from 'prop-types';
+import { arrayOf, number } from 'prop-types';
 import { Apiary } from '../propTypes';
 
-import { openSignUpModal } from '../actions';
+import GuestSurveyView from './GuestSurveyView';
+import EmptySurveyView from './EmptySurveyView';
+import SurveyView from './SurveyView';
 
-const Survey = ({ dispatch, userId, apiaries }) => {
+const Survey = ({ userId, apiaries }) => {
     if (!userId) {
-        return (
-            <div className="survey empty">
-                <div className="survey__header empty">
-                    Sign up to participate in our study to get better info about hive locations
-                </div>
-                <div className="survey__body">
-                    <div>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat.
-                    </div>
-                    <button
-                        type="button"
-                        className="survey__button"
-                        onClick={() => dispatch(openSignUpModal())}
-                    >
-                        Sign up
-                    </button>
-                </div>
-            </div>
-        );
+        return <GuestSurveyView />;
     }
 
     if (apiaries.length === 0) {
-        return (
-            <div className="survey empty">
-                <div className="survey__header empty">
-                    Add locations of interest
-                </div>
-                <div className="survey__body">
-                    <div>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat.
-                    </div>
-                    <Link to="/" className="survey__button">Go to location finder</Link>
-                </div>
-            </div>
-        );
+        return <EmptySurveyView />;
     }
 
-    return (
-        <div className="survey">
-            <div className="survey__header">
-                Survey
-            </div>
-            <div className="survey__body">
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </div>
-                <div className="survey__body--section">
-                    Response needed
-                </div>
-                <div className="survey__body--section">
-                    Up to date
-                </div>
-                <div className="survey__body--section">
-                    This is not in the study
-                </div>
-            </div>
-        </div>
-    );
+    return <SurveyView apiaries={apiaries} />;
 };
 
 function mapStateToProps(state) {
     return {
-        dispatch: state.main.dispatch,
         userId: state.auth.userId,
         apiaries: state.main.apiaries,
     };
 }
 
 Survey.propTypes = {
-    dispatch: func.isRequired,
     userId: number,
     apiaries: arrayOf(Apiary).isRequired,
 };
