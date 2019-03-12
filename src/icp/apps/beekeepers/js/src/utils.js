36f47a5b220d93b4df5251ddebbd033de63554e5
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2019-03-12 18:41:28 -0400

    Use Insecticide load in sort dropdown as well
    Refactor the sort dropdown to take an object of keys and labels,
    rather than a simple array that is transformed into labels. This
    because we want to use `pesticide` as the key but change the label
    to be `Insecticide load`.
    
    Also remove methods no longer used.

--- src/icp/apps/beekeepers/js/src/utils.js
+++ src/icp/apps/beekeepers/js/src/utils.js
@@ -10,17 +10,6 @@ export function toDashedString(value) {
     return value.toLowerCase().replace('_', '-');
 }
 
-export function toSpacedString(value) {
-    return value.replace('_', '\xa0'); // \xa0 === &nbsp;
-}
-
-export function toTitleCase(value) {
-    return value
-        .split(/(\s+)/)
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join('');
-}
-
 export function isSameLocation(a, b) {
     return a.lat === b.lat && a.lng === b.lng;
 }
