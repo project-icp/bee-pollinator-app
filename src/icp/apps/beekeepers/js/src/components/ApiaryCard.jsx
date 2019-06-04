e5d43fbc5d6e1a07b6284cee3e4a737b5a5279e5
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2019-06-04 17:45:25 -0400

    Convert ApiaryCard to Class Component
    This now needs to maintain internal state for
    hovering and editing.

--- src/icp/apps/beekeepers/js/src/components/ApiaryCard.jsx
+++ src/icp/apps/beekeepers/js/src/components/ApiaryCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { func, string } from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -15,98 +15,115 @@ import { getMarkerClass } from '../utils';
 import CardButton from './CardButton';
 import ScoresLabel from './ScoresLabel';
 
-const ApiaryCard = ({ apiary, forageRange, dispatch }) => {
-    const {
-        marker,
-        name,
-        starred,
-        surveyed,
-        scores,
-    } = apiary;
+class ApiaryCard extends Component {
+    constructor(props) {
+        super(props);
 
-    const values = scores[forageRange];
-
-    const markerClass = getMarkerClass(apiary);
-
-    const onStar = () => dispatch(setApiaryStar(apiary));
-    const onSurvey = () => dispatch(setApiarySurvey(apiary));
-    const onDelete = () => dispatch(deleteApiary(apiary));
-
-    let scoreCard;
-    if (!Object.keys(apiary.scores[forageRange]).length && !apiary.fetching) {
-        scoreCard = (
-            <>
-                <div className="card__top">
-                    <div className="card__identification">
-                        <div className={`marker ${markerClass}`}>{marker}</div>
-                        <div className="card__name">Error fetching apiary data</div>
-                    </div>
-                    <div className="card__buttons">
-                        <CardButton
-                            icon="trash"
-                            filled
-                            tooltip="Delete apiary"
-                            onClick={onDelete}
-                        />
-                    </div>
-                </div>
-                <button
-                    type="button"
-                    onClick={() => dispatch(fetchApiaryScores([apiary], forageRange))}
-                >
-                    Try again?
-                </button>
-            </>
-        );
-    } else if (!Object.keys(apiary.scores[forageRange]).length) {
-        scoreCard = <div className="spinner" />;
-    } else {
-        const scoreLabels = Object.values(INDICATORS).map(i => (
-            <ScoresLabel key={i} indicator={i} scores={[values[i]]} />
-        ));
-        scoreCard = (
-            <>
-                <div className="card__top">
-                    <div className="card__identification">
-                        <div className={`marker ${markerClass}`}>{marker}</div>
-                        <div className="card__name">{name}</div>
-                    </div>
-                    <div className="card__buttons">
-                        <CardButton
-                            icon="star"
-                            filled={starred}
-                            tooltip="Mark apiary as important"
-                            onClick={onStar}
-                        />
-                        <CardButton
-                            icon="clipboard"
-                            filled={surveyed}
-                            tooltip="Include apiary in surveys"
-                            onClick={onSurvey}
-                        />
-                        <CardButton
-                            icon="trash"
-                            filled
-                            tooltip="Delete apiary"
-                            onClick={onDelete}
-                        />
-                    </div>
-                </div>
-                <div className="card__bottom">
-                    <div className="indicator-container">
-                        {scoreLabels}
-                    </div>
-                </div>
-            </>
-        );
+        this.onStar = this.dispatchAction(setApiaryStar).bind(this);
+        this.onSurvey = this.dispatchAction(setApiarySurvey).bind(this);
+        this.onDelete = this.dispatchAction(deleteApiary).bind(this);
     }
 
-    return (
-        <li className="card">
-            {scoreCard}
-        </li>
-    );
-};
+    dispatchAction(action) {
+        return () => {
+            const { apiary, dispatch } = this.props;
+
+            dispatch(action(apiary));
+        };
+    }
+
+    render() {
+        const { apiary, forageRange, dispatch } = this.props;
+
+        const {
+            marker,
+            name,
+            starred,
+            surveyed,
+            scores,
+            fetching,
+        } = apiary;
+
+        const values = scores[forageRange];
+
+        const markerClass = getMarkerClass(apiary);
+
+        let scoreCard;
+        if (!Object.keys(scores[forageRange]).length && !fetching) {
+            scoreCard = (
+                <>
+                    <div className="card__top">
+                        <div className="card__identification">
+                            <div className={`marker ${markerClass}`}>{marker}</div>
+                            <div className="card__name">Error fetching apiary data</div>
+                        </div>
+                        <div className="card__buttons">
+                            <CardButton
+                                icon="trash"
+                                filled
+                                tooltip="Delete apiary"
+                                onClick={this.onDelete}
+                            />
+                        </div>
+                    </div>
+                    <button
+                        type="button"
+                        onClick={() => dispatch(fetchApiaryScores([apiary], forageRange))}
+                    >
+                        Try again?
+                    </button>
+                </>
+            );
+        } else if (!Object.keys(scores[forageRange]).length) {
+            scoreCard = <div className="spinner" />;
+        } else {
+            const scoreLabels = Object.values(INDICATORS).map(i => (
+                <ScoresLabel key={i} indicator={i} scores={[values[i]]} />
+            ));
+            scoreCard = (
+                <>
+                    <div className="card__top">
+                        <div className="card__identification">
+                            <div className={`marker ${markerClass}`}>{marker}</div>
+                            <div className="card__name">{name}</div>
+                        </div>
+                        <div className="card__buttons">
+                            <CardButton
+                                icon="star"
+                                filled={starred}
+                                tooltip="Mark apiary as important"
+                                onClick={this.onStar}
+                            />
+                            <CardButton
+                                icon="clipboard"
+                                filled={surveyed}
+                                tooltip="Include apiary in surveys"
+                                onClick={this.onSurvey}
+                            />
+                            <CardButton
+                                icon="trash"
+                                filled
+                                tooltip="Delete apiary"
+                                onClick={this.onDelete}
+                            />
+                        </div>
+                    </div>
+                    <div className="card__bottom">
+                        <div className="indicator-container">
+                            {scoreLabels}
+                        </div>
+                    </div>
+                </>
+            );
+        }
+
+        return (
+            <li className="card">
+                {scoreCard}
+            </li>
+        );
+    }
+}
 
 ApiaryCard.propTypes = {
     apiary: Apiary.isRequired,
