aff58b865c78c30fd59085355d853a727017ea30
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2019-06-05 16:23:31 -0400

    Enable editing apiary names
    When a user hovers on an apiary name in the card, they see
    a faint border an a pencil icon indicating that it can be
    edited. Clicking either the apiary name or the pencil icon
    makes the name editable. When the edits are complete, a
    user may hit "Enter" or the check button to save the
    changes. If there are no changes, nothing is done. Otherwise,
    the changes are sent to the server immediately and saved to
    the database.

--- src/icp/apps/beekeepers/js/src/components/ApiaryCard.jsx
+++ src/icp/apps/beekeepers/js/src/components/ApiaryCard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { func, string } from 'prop-types';
 import { connect } from 'react-redux';
+import update from 'immutability-helper';
 
 import { Apiary } from '../propTypes';
 import { INDICATORS } from '../constants';
@@ -8,6 +9,7 @@ import {
     setApiaryStar,
     setApiarySurvey,
     deleteApiary,
+    updateApiary,
     fetchApiaryScores,
 } from '../actions';
 import { getMarkerClass } from '../utils';
@@ -19,9 +21,93 @@ class ApiaryCard extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            isHovering: false,
+            isEditing: false,
+            apiaryName: props.apiary.name,
+        };
+
         this.onStar = this.dispatchAction(setApiaryStar).bind(this);
         this.onSurvey = this.dispatchAction(setApiarySurvey).bind(this);
         this.onDelete = this.dispatchAction(deleteApiary).bind(this);
+        this.onApiaryNameChange = this.onApiaryNameChange.bind(this);
+        this.onApiaryNameSave = this.onApiaryNameSave.bind(this);
+        this.getCardNameBlock = this.getCardNameBlock.bind(this);
+        this.enableHovering = this.enableHovering.bind(this);
+        this.disableHovering = this.disableHovering.bind(this);
+        this.enableEditing = this.enableEditing.bind(this);
+        this.enableEditingOnEnter = this.enableEditingOnEnter.bind(this);
+        this.saveApiaryNameOnEnter = this.saveApiaryNameOnEnter.bind(this);
+    }
+
+    onApiaryNameChange(e) {
+        this.setState({ apiaryName: e.target.value });
+    }
+
+    onApiaryNameSave() {
+        const { apiary, dispatch } = this.props;
+        const { apiaryName } = this.state;
+
+        if (apiary.name !== apiaryName) {
+            const apiaryWithNewName = update(apiary, {
+                name: { $set: apiaryName },
+            });
+
+            dispatch(updateApiary(apiaryWithNewName));
+        }
+
+        this.setState({ isEditing: false });
+    }
+
+    getCardNameBlock() {
+        const { isEditing, isHovering, apiaryName } = this.state;
+
+        if (isEditing) {
+            return (
+                <>
+                    <input
+                        className="card__name card__name-editing"
+                        onChange={this.onApiaryNameChange}
+                        onKeyPress={this.saveApiaryNameOnEnter}
+                        value={apiaryName}
+                    />
+                    <CardButton
+                        icon="check"
+                        tooltip="Save apiary name"
+                        onClick={this.onApiaryNameSave}
+                    />
+                </>
+            );
+        }
+
+        if (isHovering) {
+            return (
+                <>
+                    <input
+                        className="card__name card__name-hovering"
+                        readOnly
+                        onClick={this.enableEditing}
+                        onKeyPress={this.enableEditingOnEnter}
+                        value={apiaryName}
+                    />
+                    <CardButton
+                        icon="pencil"
+                        tooltip="Edit apiary name"
+                        onClick={this.enableEditing}
+                    />
+                </>
+            );
+        }
+
+        return (
+            <input
+                className="card__name"
+                readOnly
+                onKeyPress={this.enableEditingOnEnter}
+                onFocus={this.enableHovering}
+                value={apiaryName}
+            />
+        );
     }
 
     dispatchAction(action) {
@@ -32,12 +118,39 @@ class ApiaryCard extends Component {
         };
     }
 
+    enableHovering() {
+        this.setState({ isHovering: true });
+    }
+
+    disableHovering() {
+        this.setState({ isHovering: false });
+    }
+
+    enableEditing() {
+        this.setState({ isEditing: true });
+    }
+
+    enableEditingOnEnter(e) {
+        const { isEditing } = this.state;
+
+        if (!isEditing && e.key === 'Enter') {
+            this.setState({ isEditing: true });
+        }
+    }
+
+    saveApiaryNameOnEnter(e) {
+        const { isEditing } = this.state;
+
+        if (isEditing && e.key === 'Enter') {
+            this.onApiaryNameSave();
+        }
+    }
+
     render() {
         const { apiary, forageRange, dispatch } = this.props;
 
         const {
             marker,
-            name,
             starred,
             surveyed,
             scores,
@@ -48,10 +161,12 @@ class ApiaryCard extends Component {
 
         const markerClass = getMarkerClass(apiary);
 
-        let scoreCard;
-        if (!Object.keys(scores[forageRange]).length && !fetching) {
-            scoreCard = (
-                <>
+        if (!Object.keys(values).length && !fetching) {
+            // Fetch is complete and no scores retrieved
+            // Report error
+
+            return (
+                <li className="card">
                     <div className="card__top">
                         <div className="card__identification">
                             <div className={`marker ${markerClass}`}>{marker}</div>
@@ -72,54 +187,59 @@ class ApiaryCard extends Component {
                     >
                         Try again?
                     </button>
-                </>
-            );
-        } else if (!Object.keys(scores[forageRange]).length) {
-            scoreCard = <div className="spinner" />;
-        } else {
-            const scoreLabels = Object.values(INDICATORS).map(i => (
-                <ScoresLabel key={i} indicator={i} scores={[values[i]]} />
-            ));
-            scoreCard = (
-                <>
-                    <div className="card__top">
-                        <div className="card__identification">
-                            <div className={`marker ${markerClass}`}>{marker}</div>
-                            <div className="card__name">{name}</div>
-                        </div>
-                        <div className="card__buttons">
-                            <CardButton
-                                icon="star"
-                                filled={starred}
-                                tooltip="Mark apiary as important"
-                                onClick={this.onStar}
-                            />
-                            <CardButton
-                                icon="clipboard"
-                                filled={surveyed}
-                                tooltip="Include apiary in surveys"
-                                onClick={this.onSurvey}
-                            />
-                            <CardButton
-                                icon="trash"
-                                filled
-                                tooltip="Delete apiary"
-                                onClick={this.onDelete}
-                            />
-                        </div>
-                    </div>
-                    <div className="card__bottom">
-                        <div className="indicator-container">
-                            {scoreLabels}
-                        </div>
-                    </div>
-                </>
+                </li>
             );
         }
 
+        if (fetching) {
+            return (
+                <li className="card">
+                    <div className="spinner" />
+                </li>
+            );
+        }
+
+        const scoreLabels = Object.values(INDICATORS).map(i => (
+            <ScoresLabel key={i} indicator={i} scores={[values[i]]} />
+        ));
+
         return (
             <li className="card">
-                {scoreCard}
+                <div className="card__top">
+                    <div
+                        className="card__identification"
+                        onMouseEnter={this.enableHovering}
+                        onMouseLeave={this.disableHovering}
+                    >
+                        <div className={`marker ${markerClass}`}>{marker}</div>
+                        {this.getCardNameBlock()}
+                    </div>
+                    <div className="card__buttons">
+                        <CardButton
+                            icon="star"
+                            filled={starred}
+                            tooltip="Mark apiary as important"
+                            onClick={this.onStar}
+                        />
+                        <CardButton
+                            icon="clipboard"
+                            filled={surveyed}
+                            tooltip="Include apiary in surveys"
+                            onClick={this.onSurvey}
+                        />
+                        <CardButton
+                            icon="trash"
+                            filled
+                            tooltip="Delete apiary"
+                            onClick={this.onDelete}
+                        />
+                    </div>
+                </div>
+                <div className="card__bottom">
+                    <div className="indicator-container">
+                        {scoreLabels}
+                    </div>
+                </div>
             </li>
         );
     }
